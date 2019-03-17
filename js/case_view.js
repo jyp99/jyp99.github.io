2708ccf6b04431486731941de634441aebc66b0a
rudorpu99 <jyp@hivelab.co.kr>
2019-03-18 00:18:31 +0900

    포트폴리오 상세 양식 변경, 미완성 컨텐츠 임시 조치 적용

--- js/case_view.js
+++ js/case_view.js
@@ -3,4 +3,8 @@ $(function(){
 		e.preventDefault();
 		$('.wrap').toggleClass('is_folding');
 	});
+
+	$('.portfolio_img_wrap .btn_img_view').click(function(e){
+		$(this).parents('.portfolio_img_wrap').toggleClass('is_active');
+	});
 });
